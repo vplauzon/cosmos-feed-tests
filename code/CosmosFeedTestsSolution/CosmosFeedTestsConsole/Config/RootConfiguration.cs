6845cceac30fd8326a4888c094c333a9a16ebc81
vplauzon <vincent_lauzon@yahoo.com.au>
2022-03-09 12:32:00 -0500

    Scafolding

---
+++ code/CosmosFeedTestsSolution/CosmosFeedTestsConsole/Config/RootConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosmosFeedTestsConsole.Config
+{
+    internal class RootConfiguration
+    {
+        public string? Endpoint { get; set; }
+        
+        public string? AccessKey { get; set; }
+    }
+}
--- code/CosmosFeedTestsSolution/CosmosFeedTestsConsole/Program.cs
+++ code/CosmosFeedTestsSolution/CosmosFeedTestsConsole/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CosmosFeedTestsConsole.Config;
+using Microsoft.Azure.Cosmos;
+using System;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace CosmosFeedTestsConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.Error.WriteLine("You have to call this exec with a parameter pointing to the config file");
+            }
+            else
+            {
+                var configPath = args[0];
+                var configContent = File.ReadAllText(configPath);
+                var deserializer = new DeserializerBuilder()
+                    .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                    .Build();
+                var config = deserializer.Deserialize<RootConfiguration>(configContent);
+                var cosmosClient = new CosmosClient(config.Endpoint!, config.AccessKey!);
+
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+}
